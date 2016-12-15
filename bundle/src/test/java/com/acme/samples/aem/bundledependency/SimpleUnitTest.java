38b3b0f6207c7ad4a2140732f05ac5ed6526ecb3
Daniel Henrique Alves Lima <daniel@danielhalima.com>
2016-12-15 21:29:12 -0200

    Initial version

---
+++ bundle/src/test/java/com/acme/samples/aem/bundledependency/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.acme.samples.aem.bundledependency;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
